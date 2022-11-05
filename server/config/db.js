61f9075598f52fed5b62d38811f3207005aa17cc
nitheesh <mnc9505@gmail.com>
2022-11-06 00:52:07 +0530

    chnages in package.json mongoose version 1

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-      useCreateIndex: true,
-    })
+    const conn = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017",)
 
     console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
   } catch (error) {
